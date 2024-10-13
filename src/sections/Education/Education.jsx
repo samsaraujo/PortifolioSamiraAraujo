a6eba043622d693f6e9dc085f00228c6636d8433
Samira Araujo <133232012+samsaraujo@users.noreply.github.com>
2024-10-12 21:11:40 -0300

    ajustes em ingles

--- src/sections/Education/Education.jsx
+++ src/sections/Education/Education.jsx
@@ -9,15 +9,15 @@ function Education() {
     <section id="education" className={styles.container}>
       <h1 className="sectionTitle">EDUCATION</h1>
       <div>
-      <h2>Bacharelado em Engenharia de Software</h2>
+      <h2>Software Engineering</h2>
       <p>Unopar</p>
-      <p>2024 - Atualmente</p>
+      <p>2024 - Present</p>
       </div>
      <hr />
       <div>
-        <h2>Bacharelado em Engenharia Elétrica</h2>
+        <h2>Electrical Engineering</h2>
         <p>Universidade Federal do Ceará - Campus Sobral</p>
-        <p>2016 - Atualmente</p>
+        <p>2016 - Present</p>
       </div>
     </section>
   );
